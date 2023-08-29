8c4749837032abdce644b27796cb3f433954a17b
Sathish K Lakshmanan <60668130+sathishlakshmanan@users.noreply.github.com>
2023-08-29 15:04:00 +0200

    Create README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blueprint
+
+## Golang
+
+Download the file and move it to /usr/local/bin to execute the command from any directory<br>
+```sudo curl https://raw.githubusercontent.com/sathishlakshmanan/blueprint/main/golang/goinit.sh -o /usr/local/bin/goinit```
+
+
+Make the file an executable<br>
+```sudo chmod +x /usr/local/bin/goinit```
+
+Now, you can run `goinit` from any directory.
+
+### Example
+<img src="https://github.com/sathishlakshmanan/blueprint/assets/60668130/1bd9f4e1-74f0-474f-a2ba-9121aff2f943"  width="70%" height="30%">
--- golang/goinit.sh
+++ golang/goinit.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
-read -p 'Enter module name: ' directory_name
+directory_name=$1
 
-while [[ $directory_name = "" || $directory_name = "." ]]
-do
-    echo -e "Please enter a valid module name.\nExample usage:
-            'example.com/m' to initialize a v0 or v1 module
-            'example.com/m/v2' to initialize a v2 module"
-    read -p 'Enter module name: ' directory_name
+while getopts ":n" flag; do
+    case $flag in
+        n)
+            # get the default main.go file
+            main_go='https://raw.githubusercontent.com/sathishlakshmanan/blueprint/main/golang/main.go'
+            curl -s "$main_go" -o main.go
+
+            echo "Created a default main.go file in the current directory"
+            exit 0
+            ;;
+    esac
 done
+
+if [[ $directory_name = "" && $flag = "?" ]]
+then
+    echo -e "Usage: goinit [module_name] | [-n]\n\nExample:
+    goinit example.com/m : creates a module named \"example.com/m\" with a default main.go file
+    goinit -n : creates only a default main.go file without the module"
+    exit 0
+fi
 
 # initialize the go module
 go mod init $directory_name
@@ -18,3 +31,9 @@ main_go='https://raw.githubusercontent.com/sathishlakshmanan/blueprint/main/gola
 curl -s "$main_go" -o main.go
 
 echo "Created a default main.go file in the current directory"
+
+echo -e "\n--- go.mod ---"
+cat go.mod
+
+echo -e "\n--- main.go ---"
+cat main.go
